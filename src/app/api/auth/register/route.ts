29b042c105e1581d35d4d8c187150c6e9fc16ad8
@JosueJafet <jafethramos1204@gmail.com>
2026-06-04 23:05:52 -0600

    fix: nodejs runtime en todos los routes

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcryptjs";
+export const runtime = "nodejs";
 
 const prisma = new PrismaClient();
 
